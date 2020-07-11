8b5d679696c44e600bced091a0cfabf43ab21294
Varun Dodla <varun@dodla.com>
2020-07-10 22:29:19 -0700

    add git depth argument when looking for changes

--- files/files.go
+++ files/files.go
@@ -1,6 +1,7 @@
 package files
 
 import (
+	"fmt"
 	"log"
 	"os/exec"
 	"path/filepath"
@@ -12,11 +13,11 @@ import (
 // FindFilesInGit finds all files in git in the given root
 // matching the include pattern. optinally you can also find
 // files changed in last commit
-func FindFilesInGit(rootDirAbs, includePattern string, changedOnly bool) []string {
+func FindFilesInGit(rootDirAbs, includePattern string, changedOnly bool, gitDepth int) []string {
 	// prepare the git command
 	var cmd *exec.Cmd
 	if changedOnly {
-		cmd = exec.Command("git", "--no-pager", "diff", "--name-only", "HEAD..HEAD~10")
+		cmd = exec.Command("git", "--no-pager", "diff", "--name-only", fmt.Sprintf("HEAD..HEAD~%d", gitDepth))
 	} else {
 		cmd = exec.Command("git", "ls-tree", "-r", "HEAD", "--name-only")
 	}
--- main.go
+++ main.go
@@ -14,15 +14,16 @@ func main() {
 
 	// setup the argument parsing
 	rootDirAbs := flag.String("root", currentDir, "absolute path to root directory")
-	changedOnly := flag.Bool("changed", false, "perform git check for changed files")
 	docsDirPattern := flag.String("docs", "**/docs/", "glob pattern for doc directories")
+	changedOnly := flag.Bool("changed", false, "perform git check for changed files")
+	gitDepth := flag.Int("depth", 1, "git commit depth when looking for changes")
 
 	// parse the arguments
 	flag.Parse()
 
 	// find files that have changed since the last commit
 	mdFilePattern := filepath.Join(*docsDirPattern, "**", "*.md")
-	mdFiles := files.FindFilesInGit(*rootDirAbs, mdFilePattern, *changedOnly)
+	mdFiles := files.FindFilesInGit(*rootDirAbs, mdFilePattern, *changedOnly, *gitDepth)
 
 	for _, file := range mdFiles {
 		println(file)
